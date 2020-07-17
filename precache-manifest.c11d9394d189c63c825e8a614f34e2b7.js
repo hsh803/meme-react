4352821289ebfded13c8488e80f1692325939add
Hanna Han <hsh803@gmail.com>
2020-07-17 15:57:17 +0200

    Updates

---
+++ precache-manifest.c11d9394d189c63c825e8a614f34e2b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5df6432aba7eaaec1508259fe8cf92f2",
+    "url": "/meme-react/index.html"
+  },
+  {
+    "revision": "d337f37ddb7c3b5def7e",
+    "url": "/meme-react/static/css/main.d40e418b.chunk.css"
+  },
+  {
+    "revision": "f43b252451e05c1b66a0",
+    "url": "/meme-react/static/js/2.94a0e805.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meme-react/static/js/2.94a0e805.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d337f37ddb7c3b5def7e",
+    "url": "/meme-react/static/js/main.5f6facdc.chunk.js"
+  },
+  {
+    "revision": "1db18e8fbefe33e89de2",
+    "url": "/meme-react/static/js/runtime-main.cc4b23ab.js"
+  }
+]);
